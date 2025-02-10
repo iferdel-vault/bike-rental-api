254cf74ff299f84d062962ee535742e05129f58a
iferdel <ignaferdel@gmail.com>
2025-02-10 09:23:40 -0300

    default user hashed password to unset

--- internal/sql/schema/001_users.sql
+++ internal/sql/schema/001_users.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
 		email TEXT UNIQUE NOT NULL,
-		hashed_password TEXT NOT NULL,
+		hashed_password TEXT NOT NULL DEFAULT 'unset',
 		first_name TEXT NOT NULL,
 		last_name TEXT NOT NULL,
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
